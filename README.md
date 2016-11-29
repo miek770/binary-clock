11c23834eb655df1b82367231697c59101b662ad
Michel Lavoie <lavoie.michel@gmail.com>
2016-11-28 22:14:14 -0500

    PIC configuration in progress, timing algorithm found.

--- README.md
+++ README.md
@@ -18,3 +18,28 @@ It can be fed either or by both:
 2. A micro-USB port.
 
 Both supplies are paralleled with diodes, and the PIC monitors the batteries' voltage so that an alarm (blinking LED) is raised when the batteries are low. For 1.5V AA batteries, this would be pretty much when they get under 1V.
+
+## Timing
+
+I switched from the PIC18F252 to the PIC18F2220 because the former doesn't have an internal oscillator. I assumed it did.
+
+I will be using TMR0 to count seconds, using a bresenham algorithm as explained and implemented here: [http://www.romanblack.com/one_sec.htm](http://www.romanblack.com/one_sec.htm)
+
+Basically (this is all from his website above):
+
+    This next one is similar to the 1 second generators above. However it is optimised for speed and code space, because it uses a 16bit unsigned int variable for bres instead of a 32bit unsigned long. We do this by using values for the int period and the 1second period that are in the same ratio to each other, but smaller! We divide both values by 16;
+
+    Optimised C code for a 1 second period with a 1MHz timer (4MHz xtal);
+
+        // uses 1 variable; unsigned int bres
+        // gets here every TMR0 int (every 256 ticks)
+
+        bres += 16; // add (256/16) ticks to bresenham total
+
+        if(bres >= 62500)   // if reached (1000000/16) 1 second!
+        {
+            bres -= 62500;  // subtract 1 second, retain error
+            do_1sec_event();    // update clock, etc
+        }
+
+This is really quite neat!
--- mplab/main.c
+++ mplab/main.c
@@ -1,12 +1,55 @@
 #include <p18f2220.h>
 
 #pragma config OSC = INTIO2
+#pragma config WDT = OFF
 
-void main()
+#pragma code INTERRUPT_VECTOR = 0x8
+void ISR (void)
 {
-	TRISA = 0b
-    while(1)
-    {
-        pass;
-    }
+	// See http://www.romanblack.com/one_sec.htm for method of making a 1s period
+}
+
+#pragma code
+
+void main (void)
+{
+	// Oscillator configuration
+	OSCCONbits.IDLEN = 1; // Idle mode enabled; CPU core is not clocked in power managed modes
+	OSCCONbits.IRCF2 = 0; // 31 kHz (INTRC source drives clock directly, check if 31.25 kHz) 
+	OSCCONbits.IRCF1 = 0;
+	OSCCONbits.IRCF0 = 0;
+	OSCCONbits.SCS1 = 1; // Internal oscillator block (RC modes)
+
+	// Interrupts
+	RCONbits.IPEN = 0; // Disable interrupt priority
+	INTCONbits.GIE = 1; // Enable all interrupt sources
+	INTCONbits.PEIE = 1; // Enable peripheral interrupt sources
+	INTCONbits.TMR0IE = 1; // Enable TMR0 interrupt
+	INTCONbits.INT0IE = 1; // Enable INT0 interrupt
+	INTCON2bits.INTEDG0 = 1; // INT0 on rising edge
+	INTCON3bits.INT1IE = 0; // Enable INT1 interrupt
+	INTCON2bits.INTEDG1 = 1; // INT1 on rising edge
+	PIE1bits.ADIE = 1;
+
+	// Timer0
+	T0CONbits.TMR0ON = 1; // Enable TMR0
+	T0CONbits.T08BIT = 0; // 16-bit timer (65536)
+	T0CONbits.T0CS = 0; // Internal instruction cycle clock
+	T0CONbits.PSA = ; // 
+	T0CONbits. = ; // 
+	T0CONbits. = ; // 
+	T0CONbits. = ; // 
+
+	// A/D converter
+	ADCON0
+	ADCON1
+	ADCON2
+
+	// Ports A,B,C
+	TRISA = 0b00000001;
+	PORTA = 0x00;
+	TRISB = 0b11110011;
+	PORTB = 0x00;
+	TRISC = 0b00000000;
+	PORTC = 0x00;
 }
